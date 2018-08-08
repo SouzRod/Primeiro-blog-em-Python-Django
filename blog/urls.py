a264a304400e037c07f998187a0b487bd9d1547c
souzrod <rdrg.rcs1997@gmail.com>
2018-08-08 16:41:21 -0300

    arrumei as urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from blog import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/(?P<pk>[0-9]+)', views.post_detail, name='post_detail'),
+    path('post/(?P<pk>[0-9]+)$', views.post_detail, name='post_detail'),
     path('post/new/$', views.post_new, name='post_new'),
     path('post/(?P<pk>\d+)/edit/', views.post_edit, name='post_edit'),
 ]
