f3142a2aac16d91b550ca4e8c8dacebc723ff6f2
souzrod <rdrg.rcs1997@gmail.com>
2018-08-09 11:24:11 -0300

    Escondi o botão de adicionar post para quem não é adm

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from blog import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/(?P<pk>[0-9]+)$', views.post_detail, name='post_detail'),
-    path('post/new/$', views.post_new, name='post_new'),
+    path('post/new/', views.post_new, name='post_new'),
     path('post/(?P<pk>\d+)/edit/', views.post_edit, name='post_edit'),
 ]
